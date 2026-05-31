e98565c1a8f069fbb1dccd98a406c4ac00495116
Galib <217484167+galib-i@users.noreply.github.com>
2026-05-31 19:42:00 +0100

    fix: disable when on other tabs

--- background.js
+++ background.js
@@ -1,16 +1,24 @@
 const manifest = chrome.runtime.getManifest();
 const allowedHosts = manifest.host_permissions || [];
+const cleanHosts = allowedHosts.map((h) => h.replace("/*", ""));
 
-const cleanHosts = allowedHosts.map((host) => host.replace("/*", ""));
+function updateActionForTab(tabId, url) {
+  const isAllowed = !!url && cleanHosts.some((h) => url.startsWith(h));
+  if (isAllowed) chrome.action.enable(tabId);
+  else chrome.action.disable(tabId);
+}
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (!tab.url) return;
-
-  const isAllowedSite = cleanHosts.some((host) => tab.url.startsWith(host));
-
-  if (isAllowedSite) {
-    chrome.action.enable(tabId);
-  } else {
-    chrome.action.disable(tabId);
-  }
+  const url = changeInfo.url || tab?.url || "";
+  updateActionForTab(tabId, url);
 });
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  chrome.tabs.get(activeInfo.tabId, (tab) =>
+    updateActionForTab(tab.id, tab.url),
+  );
+});
+
+chrome.tabs.query({}, (tabs) =>
+  tabs.forEach((t) => updateActionForTab(t.id, t.url)),
+);
